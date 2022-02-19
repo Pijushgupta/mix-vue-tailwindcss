92589a6ac7ef812efe78642eb0010a21f8e16598
Pijush Gupta <Pijush@live.com>
2022-02-19 19:18:19 +0530

    vue loader issue fix for .vue file

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,3 @@
 let mix = require('laravel-mix');
-mix.js('assets/src/app.js', 'assets/dist');
+mix.js('assets/src/app.js', 'assets/dist').vue();
 mix.postCss('assets/src/app.css', 'assets/dist', [require("tailwindcss"),]);
